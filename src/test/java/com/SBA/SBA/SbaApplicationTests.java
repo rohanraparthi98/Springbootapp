7421ea682d968207e8c5e4ce35a665eb6e30cbe6
rohanraparthi98 <rohanraparthi@gmail.com>
2023-05-26 03:17:35 +0530

    springboot files added

---
+++ src/test/java/com/SBA/SBA/SbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SBA.SBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
